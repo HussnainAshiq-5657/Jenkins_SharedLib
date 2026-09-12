32eec65bb654da24337718ded9d3cc99955a6781
Hussnain Ashiq <hussnainnaz3@gmail.com>
2026-09-12 15:40:53 +0500

    Implement conditional .env file creation
    Add environment variable setup for wanderlust-frontend-beta

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,7 @@
 // Define function
 def call(String ProjectName, String ImageTag, String DockerHubUser){
+  if (ProjectName == "wanderlust-frontend-beta") {
+        sh 'echo \'VITE_API_PATH="http://13.58.214.45:31100"\' > .env.docker'
+    }
   sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
 }
